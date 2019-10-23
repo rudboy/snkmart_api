96dce435a38f939716d19f8262164a412ae4f61b
RuddyMJ <rudboy972@gmail.com>
2019-10-24 01:19:21 +0200

    post_notifacation

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -103,8 +103,7 @@ router.post("/post_comment", isAuthenticated, async (req, res) => {
       date: date
     });
     if (req.user._id !== info_post.creator._id) {
-      const creator = await USER.findOne({ _id: info_post.creator._id });
-      res.json(creator);
+      let creator = await USER.findOne({ _id: info_post.creator._id });
       creator.notification.push({
         id: req.user._id,
         username: req.user.username,
