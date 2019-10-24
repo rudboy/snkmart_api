09e94d9cecc545aa155693d8f798f3d8766d6109
RuddyMJ <rudboy972@gmail.com>
2019-10-24 22:30:19 +0200

    delete_commente

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -171,6 +171,8 @@ router.post("/login", async (req, res) => {
 router.get("/get_my_user_info", isAuthenticated, async (req, res) => {
   try {
     const allinfo = await USER.findOne({ _id: req.user._id });
+    delete allinfo.salt;
+    delete allinfo.token;
     res.json(allinfo);
   } catch (error) {
     res.status(400).json({ error: { message: error.message } });
