adda17c6b84f8790a94fe9f72e20c3e00eba4806
RuddyMJ <rudboy972@gmail.com>
2019-10-25 00:58:13 +0200

    delete_commente

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -128,10 +128,9 @@ router.post("/delete_comment", isAuthenticated, async (req, res) => {
   try {
     let newPost = await POST.findOne({ _id: req.body.post });
     for (let i = 0; i < newPost.comment.length; i++) {
-      res.json({ date: newPost.comment[i].creator, date1: id });
       if (
-        newPost.comment[i].date === date &&
-        newPost.comment[i].creator === id
+        String(newPost.comment[i].date) === date &&
+        String(newPost.comment[i].creator) === id
       ) {
         newPost.comment.splice(i, 1);
         newPost.save();
