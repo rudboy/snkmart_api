14038db8e1cba2fdb7223c63408a7ff7e46336b3
Ruddy MJ <46441046+rudboy@users.noreply.github.com>
2020-12-15 18:46:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const postRoutes = require("./routes/postRoutes");
 const cors = require("cors");
+const axios = require("axios");
 const compression = require("compression");
 const helmet = require("helmet");
 const app = express();
@@ -19,16 +20,27 @@ app.use(
   helmet(),
   compression()
 );
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/Sneaker_Mart",
-  {
-    useNewUrlParser: true
-  }
-);
-mongoose.set("useCreateIndex", true);
+//mongoose.connect(
+//  process.env.MONGODB_URI || "mongodb://localhost/Sneaker_Mart",
+//  {
+//    useNewUrlParser: true
+//  }
+//);
+//mongoose.set("useCreateIndex", true);
 
 app.get("/", (req, res) => {
   res.json("Welcome On API SneakerMart");
+});
+
+app.post("/toto", async (req, res) => {
+  try {
+    const getLoginpage = await axios.get(
+      "https://www.instagram.com/accounts/login/"
+    );
+    console.log(getLoginpage);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 app.listen(process.env.PORT || 5500, () => {
